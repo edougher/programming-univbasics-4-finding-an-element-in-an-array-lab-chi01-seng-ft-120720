14622d03ad3c415183ac5829ad320502fcf4a84b
erin dougher <ejdougher@gmail.com>
2020-11-25 01:34:11 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,9 @@
-=begin
+
 def find_element_index(array, value_to_find)
 n = 0
 while n < array.length do
   if array[n] == value_to_find
-    p n
+    return n
 end
   n += 1
 end
@@ -12,7 +12,7 @@ end
 scale = [2,3,4,5]
 value_to_find = 2
 find_element_index(scale, value_to_find)
-=end
+
 
 def find_element_index(array, value_to_find)
   count = 0
